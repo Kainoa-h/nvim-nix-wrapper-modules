293f1d5c295ce651470c7c5fb4f5769cbfc50375
kainoa-h <kainoahowj@gmail.com>
2026-07-03 16:44:44 +0800

    fix: firenvim settings

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -2,17 +2,15 @@ return {
 	"firenvim",
 	auto_enable = true,
 	lazy = not vim.g.started_by_firenvim,
-	before = function(_)
-		vim.g.firenvim_config = {
-			globalSettings = {
-				takeover = "never",
-			},
-		}
-	end,
 	after = function(_)
 		if not vim.g.firenvim_install_done then
 			vim.fn["firenvim#install"](0)
 			vim.g.firenvim_install_done = true
 		end
+		vim.g.firenvim_config = {
+			globalSettings = {
+				takeover = "never",
+			},
+		}
 	end,
 }
