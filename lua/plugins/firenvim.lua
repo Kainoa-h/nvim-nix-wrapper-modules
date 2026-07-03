5f9d8e784315eb2cd0cdee01fe2ff6e7ae26d802
kainoa-h <kainoahowj@gmail.com>
2026-07-03 16:29:29 +0800

    feat(firenvim): more robust implementation

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -1,7 +1,14 @@
 return {
 	"firenvim",
 	auto_enable = true,
-	lazy = false,
+	lazy = not vim.g.started_by_firenvim,
+	before = function(_)
+		vim.g.firenvim_config = {
+			globalSettings = {
+				takeover = "never",
+			},
+		}
+	end,
 	after = function(_)
 		if not vim.g.firenvim_install_done then
 			vim.fn["firenvim#install"](0)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,90 +1,90 @@
-return {
+local specs = {
 	{ import = "plugins.colorschemes" },
-	{ import = "plugins.snacks" },
-	{ import = "plugins.lsp" },
-	{ import = "plugins.treesitter" },
-	{ import = "plugins.conform" },
-	{ import = "plugins.lint" },
-	{ import = "plugins.blink" },
-	{ import = "plugins.lualine" },
-	{ import = "plugins.noice" },
-	{ import = "plugins.gitsigns" },
 	{ import = "plugins.flash" },
-	{ import = "plugins.bufferline" },
 	{ import = "plugins.firenvim" },
-	{
-		"colorful-menu.nvim",
-		auto_enable = true,
-		on_plugin = { "blink.cmp" },
-	},
 	{
 		"nvim-surround",
 		auto_enable = true,
 		event = "DeferredUIEnter",
-		-- keys = "",
 		after = function(plugin)
 			require("nvim-surround").setup()
 		end,
 	},
-	{
-		"vim-startuptime",
-		auto_enable = true,
-		cmd = { "StartupTime" },
-		before = function(_)
-			vim.g.startuptime_event_width = 0
-			vim.g.startuptime_tries = 10
-			vim.g.startuptime_exe_path = nixInfo(vim.v.progpath, "progpath")
-		end,
-	},
-	{
-		"fidget.nvim",
-		auto_enable = true,
-		event = "DeferredUIEnter",
-		-- keys = "",
-		after = function(plugin)
-			require("fidget").setup({})
-		end,
-	},
-	{
-		"mini.files",
-		auto_enable = true,
-		event = "DeferredUIEnter",
-		-- keys = "",
-		after = function(plugin)
-			require("mini.files").setup({})
-			vim.keymap.set("n", "<leader>fm", "<cmd>lua MiniFiles.open()<CR>", { desc = "Mini Files" })
-		end,
-	},
-	{
-		"which-key.nvim",
-		auto_enable = true,
-		-- cmd = { "" },
-		event = "DeferredUIEnter",
-		-- ft = "",
-		-- keys = "",
-		-- colorscheme = "",
-		after = function(plugin)
-			require("which-key").setup({})
-			require("which-key").add({
-				{ "<leader><leader>", group = "buffer commands" },
-				{ "<leader><leader>_", hidden = true },
-				{ "<leader>c", group = "[c]ode" },
-				{ "<leader>c_", hidden = true },
-				{ "<leader>d", group = "[d]ocument" },
-				{ "<leader>d_", hidden = true },
-				{ "<leader>g", group = "[g]it" },
-				{ "<leader>g_", hidden = true },
-				{ "<leader>m", group = "[m]arkdown" },
-				{ "<leader>m_", hidden = true },
-				{ "<leader>r", group = "[r]ename" },
-				{ "<leader>r_", hidden = true },
-				{ "<leader>s", group = "[s]earch" },
-				{ "<leader>s_", hidden = true },
-				{ "<leader>t", group = "[t]oggles" },
-				{ "<leader>t_", hidden = true },
-				{ "<leader>w", group = "[w]orkspace" },
-				{ "<leader>w_", hidden = true },
-			})
-		end,
-	},
 }
+
+if not vim.g.started_by_firenvim then
+	vim.list_extend(specs, {
+		{ import = "plugins.snacks" },
+		{ import = "plugins.lsp" },
+		{ import = "plugins.treesitter" },
+		{ import = "plugins.conform" },
+		{ import = "plugins.lint" },
+		{ import = "plugins.blink" },
+		{ import = "plugins.lualine" },
+		{ import = "plugins.noice" },
+		{ import = "plugins.gitsigns" },
+		{ import = "plugins.bufferline" },
+		{
+			"colorful-menu.nvim",
+			auto_enable = true,
+			on_plugin = { "blink.cmp" },
+		},
+		{
+			"vim-startuptime",
+			auto_enable = true,
+			cmd = { "StartupTime" },
+			before = function(_)
+				vim.g.startuptime_event_width = 0
+				vim.g.startuptime_tries = 10
+				vim.g.startuptime_exe_path = nixInfo(vim.v.progpath, "progpath")
+			end,
+		},
+		{
+			"fidget.nvim",
+			auto_enable = true,
+			event = "DeferredUIEnter",
+			after = function(plugin)
+				require("fidget").setup({})
+			end,
+		},
+		{
+			"mini.files",
+			auto_enable = true,
+			event = "DeferredUIEnter",
+			after = function(plugin)
+				require("mini.files").setup({})
+				vim.keymap.set("n", "<leader>fm", "<cmd>lua MiniFiles.open()<CR>", { desc = "Mini Files" })
+			end,
+		},
+		{
+			"which-key.nvim",
+			auto_enable = true,
+			event = "DeferredUIEnter",
+			after = function(plugin)
+				require("which-key").setup({})
+				require("which-key").add({
+					{ "<leader><leader>", group = "buffer commands" },
+					{ "<leader><leader>_", hidden = true },
+					{ "<leader>c", group = "[c]ode" },
+					{ "<leader>c_", hidden = true },
+					{ "<leader>d", group = "[d]ocument" },
+					{ "<leader>d_", hidden = true },
+					{ "<leader>g", group = "[g]it" },
+					{ "<leader>g_", hidden = true },
+					{ "<leader>m", group = "[m]arkdown" },
+					{ "<leader>m_", hidden = true },
+					{ "<leader>r", group = "[r]ename" },
+					{ "<leader>r_", hidden = true },
+					{ "<leader>s", group = "[s]earch" },
+					{ "<leader>s_", hidden = true },
+					{ "<leader>t", group = "[t]oggles" },
+					{ "<leader>t_", hidden = true },
+					{ "<leader>w", group = "[w]orkspace" },
+					{ "<leader>w_", hidden = true },
+				})
+			end,
+		},
+	})
+end
+
+return specs
