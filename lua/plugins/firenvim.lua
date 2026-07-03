6587b9e9494e732bacea245f95b52f4084293b7e
kainoa-h <kainoahowj@gmail.com>
2026-07-03 16:02:45 +0800

    fix: load firevim instantly

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -1,7 +1,7 @@
 return {
 	"firenvim",
 	auto_enable = true,
-	event = "DeferredUIEnter",
+	lazy = false,
 	after = function(_)
 		if not vim.g.firenvim_install_done then
 			vim.fn["firenvim#install"](0)
